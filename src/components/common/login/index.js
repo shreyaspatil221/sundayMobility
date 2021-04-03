332d746454d7aa562e22652e69d36a63735c71a8
ShreyasPatil <shreyas2.patil@ril.com>
2021-04-03 20:46:48 +0530

    facebook app id change

--- src/components/common/login/index.js
+++ src/components/common/login/index.js
@@ -80,7 +80,7 @@ const Login = (props) => {
       </form>
       <button type="button" css={registerBtn} onClick={toggleAuth}>{t('register')}</button>
       <FacebookLogin
-        appId="481486442768764"
+        appId="774977839795707"
         fields="name,email,picture"
         callback={faceBookLogin}
         autoLoad={false}
